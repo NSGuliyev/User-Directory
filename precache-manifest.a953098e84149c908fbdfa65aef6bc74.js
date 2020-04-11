c5112ac982502067099331dc013239f12b56b031
NSGuliyev <nesimi018@yahoo.com>
2020-04-11 11:38:03 -0400

    Updates

---
+++ precache-manifest.a953098e84149c908fbdfa65aef6bc74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f82121f6f5963130629b89a2235761",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "1d104a656c9f337b4870",
+    "url": "/User-Directory/static/js/2.e7658641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.e7658641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164749196b0e54ac4ed1",
+    "url": "/User-Directory/static/js/main.8c72c0c2.chunk.js"
+  },
+  {
+    "revision": "4f8bec5782508c197d72",
+    "url": "/User-Directory/static/js/runtime-main.12bdc3aa.js"
+  }
+]);
